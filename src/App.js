fce84e09f39d1a99b1f89a9369e6d103578f2bec
Igor Souza <igorsouza.dev@gmail.com>
2019-08-05 16:51:56 -0300

    Adding game configuration file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './params';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.welcome}>Starting the game!</Text>
+      <Text style={styles.instructions}>
+        Grid size: {params.getRowsAmount()} X {params.getColumnsAmount()}
+        </Text>
     </View>
   );
 }
@@ -16,4 +20,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  instructions: {
+
+  }
 });
