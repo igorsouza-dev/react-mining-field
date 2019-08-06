666708ad75ecdb4ca41547c2effc932d97567bd1
Igor Souza <igorsouza.dev@gmail.com>
2019-08-06 20:59:53 -0300

    Added the flag component

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ export default function App() {
       <Field mined/>
       <Field mined opened/>
       <Field mined opened exploded/>
+      <Field flagged/>
+      <Field flagged opened/>
     </View>
   );
 }
