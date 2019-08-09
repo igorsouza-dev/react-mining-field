de7516ad8241db734a9c5aff5d7b0f4ec5945f08
Igor Souza <igorsouza.dev@gmail.com>
2019-08-09 17:26:52 -0300

    Adding game logic functions

--- src/functions.js
+++ src/functions.js
@@ -12,7 +12,7 @@ const createBoard = (rows, columns) => {
             }
         })
     });
-}
+};
 
 const spreadMines = (board, minesAmount) => {
     const rows = board.length;
@@ -28,12 +28,58 @@ const spreadMines = (board, minesAmount) => {
             plantedMines++;
         }
     }
-}
+};
 
 const createMinedBoard = (rows, columns, minesAmount) => {
     const board = createBoard(rows, columns);
     spreadMines(board, minesAmount);
     return board;
+};
+
+const cloneBoard = board => {
+    return board.map(rows => {
+        return rows.map(field => {
+            return {...field};
+        });
+    });
+};
+
+const getNeighbours = (board, row, column) => {
+    const neighbours = [];
+    const rows = [row - 1, row, row + 1];
+    const columns = [column - 1, column, column + 1];
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const different = r !== row || c !== column;
+            const validRow = r >= 0 && r < board.length;
+            const validColumn = c >= 0 && c < board[0].length;
+            if (different && validRow && validColumn) {
+                neighbours.push(board[r][c]);
+            }
+        });
+    });
+    return neighbours;
+};
+
+const safeNeighbourhood = (board, row, column) => {
+    const safes = (result, neighbour) => result && !neighbour.mined;
+    return getNeighbours(board, row, column).reduce(safes, true);
+};
+
+const openField = (board, row, column) => {
+    const field = board[row][column];
+    if (!field.opened) {
+        field.opened = true;
+        if (field.mined) {
+            field.exploded = true;
+        } else if (safeNeighbourhood(board, row, column)) {
+            getNeighbours(board, row, column)
+                .forEach(n => openField(board, n.row, n.column));
+        } else {
+            const neighbours = getNeighbours(board, row, column);
+            field.nearMines = neighbours.filter(n => n.mined).length;
+        }
+    }
 }
 
 export { createMinedBoard };
